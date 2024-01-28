ad6f81450509c34bd0a72bc9962b4eafb8f60b40
Edoardo Cavallo <iweek16@gmail.com>
2024-01-28 12:58:57 +0100

    Refactoring to TS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Header from '@/components/Header'
+
 import Image from "next/image";
 
 export default function Home() {
@@ -8,17 +8,17 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
 
       <main>
-        <Header title="Matrimonio di     Tiziano e Martina" />
+        <Header title="Matrimonio di Tiziano e Martina"/>
         <p className="description">
-            <Image src={"/work-penguin.gif"} alt={"Work in progress"} width={200} height={200}/>
+          <Image src={"/work-penguin.gif"} alt={"Work in progress"} width={200} height={200}/>
         </p>
       </main>
 
       {/*<Footer />*/}
     </div>
-  )
+  );
 }
