ab295ee8bdb87aa9602190debb103f03fac0a1dd
Edoardo Cavallo <iweek16@gmail.com>
2024-02-11 19:55:50 +0100

    Fix URL fetchTexts NextJs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,15 @@ import { promises as fs } from 'fs';
 import HeroBanner from "@/components/HeroBannerProps";
 import style from './page.module.css'
 
-export default async function Home() {
-  const file = await fs.readFile(process.cwd() + '/src/data/texts.json', 'utf8');
-  const data = JSON.parse(file);
+const fetchTexts = async ()=>{
+  let baseURL = process?.env?.APP_URL
+  if(baseURL == undefined ) baseURL = process.cwd()
+  const file = await fs.readFile(baseURL + '/src/data/texts.json', 'utf8');
+  return JSON.parse(file);
+}
 
-/*
-  //<editor-fold desc="page.tsx > Home - line 12 at 11/02/2024 11:13:10">
-  console.group('page.tsx > Home - line 12 at 11/02/2024 11:13:10');
-  console.log(data);
-  console.groupEnd();
-  //</editor-fold>*/
+export default async function Home() {
+  const data = await fetchTexts()
 
   return (
     <>
