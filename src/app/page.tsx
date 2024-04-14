b8bfa40e4952c90a86e31dc917197c277c91b2ef
Edoardo Cavallo <e.cavallo@reply.it>
2024-04-14 11:17:13 +0200

    Refactor structure theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,40 +6,28 @@ import IndicazioniStradaliBanner from "@/components/sections/IndicazioniStradali
 import PerChiVieneDaFuori from "@/components/sections/PerChiVieneDaFuori";
 import NeiDintorni from "@/components/sections/NeiDintorni";
 import Partecipazione from "@/components/sections/Partecipazione";
-import data from '@/data/data.json'
 import dynamic from "next/dynamic";
+
 const ScrollTop = dynamic(() => import("@/components/ScrollTop"), {ssr: false});
 const CustomAppBar = dynamic(() => import("@/components/CustomAppBar"), {ssr: false});
-import {createTheme, ThemeProvider} from "@mui/material";
 
-const theme = createTheme({
-	palette: {
-		primary: {
-			main: '#158e83',
-		},
-		secondary: {
-			main: '#e2f0ed',
-		},
-	},
-});
 
 export default async function Home() {
 
 	return (
 		<>
-			<ThemeProvider theme={theme}>
-				{/*<Header></Header>*/}
-				<CustomAppBar/>
-				<main className={style.wrapper}>
-					<HeroBanner/>
+			{/*<Header></Header>*/}
+			<CustomAppBar/>
+			<main className={style.wrapper}>
+				<HeroBanner/>
 
-					<IndicazioniStradaliBanner/>
-					<PerChiVieneDaFuori/>
-					<NeiDintorni/>
-					{/*<Partecipazione/>*/}
-				</main>
-				<ScrollTop/>
-			</ThemeProvider>
+				<IndicazioniStradaliBanner/>
+				<PerChiVieneDaFuori/>
+				<NeiDintorni/>
+				{/*<Partecipazione/>*/}
+			</main>
+			<ScrollTop/>
+
 		</>
 	);
 }
